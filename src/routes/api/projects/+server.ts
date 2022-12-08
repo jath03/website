d499fc144f791078fb4149bd6aa84ba45734db25
jath03 <jackthughes@yahoo.com>
2022-12-07 20:40:53 -0700

    Fixing project api

--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -3,12 +3,12 @@ import type ProjectItem from '$lib/projectItem';
 
 const KEY = "projects";
 
-export const GET: RequestHandler = async ({ platform }) => {
-    let projects = await platform.env.projects_portfolio.get(KEY);
+export const get: RequestHandler = async ({ platform }) => {
+    let projects: Array<ProjectItem> | null = await platform.env.projects_portfolio.get(KEY);
     if (projects == null) {
-        console.log(await platform.env.projects_portfolio.put(KEY, `[]`));
+        await platform.env.projects_portfolio.put(KEY, `[]`);
         projects = await platform.env.projects_portfolio.get(KEY);
     }
 
-    return new Response(`{ "projects": ${JSON.stringify(projects)} }`);
+    return new Response(`{ "projects": ${JSON.stringify(projects)} }`, { headers: { 'access-control-allow-origin': '*' } });
 }
