936b0e41248af627b4f84b7a9cdc51311ec31216
jath03 <jackthughes@yahoo.com>
2022-12-07 19:41:01 -0700

    Fix project api

--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -3,20 +3,13 @@ import type { RequestHandler } from './$types';
 import type ProjectItem from '$lib/projectItem';
 
 export const GET: RequestHandler = async ({ platform }) => {
-    // await platform.env.projects_portfolio.delete("projects");
     let projects: Array<ProjectItem> = await platform.env.projects_portfolio.get("projects");
-    if (!projects || projects.length < 1) {
-        await platform.env.projects_portfolio.put("projects", `[{
-            "name": "Uno",
-            "short_description": "A terminal-based Uno game",
-            "long_description": "This was my final project from my Introductory C programming course.  We were tasked with creating the game Uno in C.",
-            "image": "uno-example.png"
-        }]`);
+    if (!projects) {
+        await platform.env.projects_portfolio.put("projects", `[]`);
         projects = await platform.env.projects_portfolio.get("projects");
     }
 
     return new Response(`{
-        "message": "hi",
         "projects": ${JSON.stringify(projects)}
     }`);
 }
