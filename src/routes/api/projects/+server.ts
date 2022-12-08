2eb050e8389b045b23e55b9a3c0e6c7d927bc297
jath03 <jackthughes@yahoo.com>
2022-12-07 19:59:42 -0700

    Debugging project api

--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -2,11 +2,13 @@
 import type { RequestHandler } from './$types';
 import type ProjectItem from '$lib/projectItem';
 
+const KEY = "projects";
+
 export const GET: RequestHandler = async ({ platform }) => {
-    let projects: Array<ProjectItem> | null = await platform.env.projects_portfolio.get("projects");
+    let projects = await platform.env.projects_portfolio.get(KEY);
     if (projects == null) {
-        await platform.env.projects_portfolio.put("projects", `[]`);
-        projects = await platform.env.projects_portfolio.get("projects");
+        await platform.env.projects_portfolio.put(KEY, `[]`);
+        projects = await platform.env.projects_portfolio.get(KEY);
     }
 
     return new Response(`{
