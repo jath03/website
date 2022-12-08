88688a3402e48b972a308863e5f0787dc7c81926
jath03 <jackthughes@yahoo.com>
2022-12-07 19:47:34 -0700

    Fix project api take 3

--- src/routes/api/projects/+server.ts
+++ src/routes/api/projects/+server.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from './$types';
 import type ProjectItem from '$lib/projectItem';
 
 export const GET: RequestHandler = async ({ platform }) => {
-    let projects: Array<ProjectItem> = await platform.env.projects_portfolio.get("projects");
+    let projects: Array<ProjectItem> | null = await platform.env.projects_portfolio.get("projects");
     if (projects == null) {
         await platform.env.projects_portfolio.put("projects", `[]`);
         projects = await platform.env.projects_portfolio.get("projects");
