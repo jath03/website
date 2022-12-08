e003de17090956e45a32f6dcb0b06bf4900e39a0
jath03 <jackthughes@yahoo.com>
2022-12-07 19:50:30 -0700

    Debug project api

--- src/routes/projects/+page.ts
+++ src/routes/projects/+page.ts
@@ -3,7 +3,9 @@ import type ProjectItem from '$lib/projectItem';
 
 export const load: PageLoad = async ({ fetch }) => {
     const res = await fetch(`/api/projects`);
+    console.log(res);
     const item = await res.json()
+    console.log(item);
     const projects: Array<ProjectItem> = JSON.parse(item.projects);
     console.log(projects);
     return { projects: projects };
