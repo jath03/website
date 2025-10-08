787d6e18a9fc633693d58820b708e7b679dc8fac
jath03 <jackthughes@yahoo.com>
2025-10-07 20:39:39 -0500

    Squashed everything

---
+++ src/lib/basicLink.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let href: string;
+</script>
+
+<a class="text-base text-sky-600 font-bold underline hover:text-black" {href}><slot /></a>
---
+++ src/lib/markdown.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import SvelteMarkdown from 'svelte-markdown';
+	import BasicLink from './basicLink.svelte';
+	export let content: string;
+</script>
+
+<SvelteMarkdown source={content} renderers={{ link: BasicLink }} isInline />
